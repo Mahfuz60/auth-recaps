07d8de830cd67cf0b8aa736c3bcfdbbd93c09eb6
Mahfuz <you@example.com>
2021-03-16 01:35:30 +0600

    code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "./firebase.config";
+import { useState } from "react";
+!firebase.apps.length && firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+  const fbProvider = new firebase.auth.FacebookAuthProvider();
+  const ghProvider = new firebase.auth.GithubAuthProvider();
+
+  const [user, setUser] = useState({});
+  const handleSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(googleProvider)
+      .then((result) => {
+        var credential = result.credential;
+        var token = credential.accessToken;
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        console.log(errorMessage, errorCode, email);
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+  };
+  const handleFbSignIN = () => {
+    firebase
+      .auth()
+      .signInWithPopup(fbProvider)
+      .then((result) => {
+        var credential = result.credential;
+        const user = result.user;
+        var accessToken = credential.accessToken;
+        setUser(user);
+        console.log("fb used sign in", user);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        console.log(errorMessage, errorCode, email);
+        var credential = error.credential;
+      });
+  };
+
+  const handleGithubSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(ghProvider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        console.log('github sign In',user);
+        
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        let email = error.email;
+        console.log('Error:',errorMessage, errorCode, email);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleSignIn}>Sign in Google</button>
+      <br /> <br />
+      <button onClick={handleFbSignIN}>Sign in Facebook</button>
+      <br /> <br />
+      <button onClick={handleGithubSignIn}>Sign in Github</button>
+      <br />
+      <h4>Name:{user.displayName}</h4>
+      <h4>Email:{user.email}</h4>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAV9du7iZq2vNgZO58t-zXBSoG_M7DzxYA",
+    authDomain: "auth-recaps.firebaseapp.com",
+    projectId: "auth-recaps",
+    storageBucket: "auth-recaps.appspot.com",
+    messagingSenderId: "1020292973557",
+    appId: "1:1020292973557:web:3637f3e84d05fafa6c56bc"
+  };
+  export default firebaseConfig;
